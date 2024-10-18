c9916a53921a00cff5242029ef3d321cc2ff1f7b
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-18 12:37:53 +0200

    add function

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
+const postEl = document.querySelector('.postit');
+console.log(postEl);
+
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
-.then (response =>{
-    const data = response.data
-    console.log(data);
+.then(response =>{
+    const postIts = response.data
+    console.log(postIts);
+
+    let postItsEl = ''
+    postIts.forEach(postIt => {
+        
+        const {url, title} = postIt
+
+
+        const markup =  ` 
+        <div class="col-xs-12 postit">
+            <div class="pin"><img src="./assets/img/pin.svg" alt=""></div>
+            <img src="${url}" alt="">
+            <div class="div cardtext">${title}</div>
+        </div>
+        `
+        postItsEl += markup
+    });
+        postEl.innerHTML = postItsEl
+        console.log(postEl);
+        
 })
+
