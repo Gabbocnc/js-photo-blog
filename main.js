45bcf23360abfdd28185a0b6c857c085117ee434
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-21 11:43:31 +0200

    add modalbackground

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
 const myRowEl = document.querySelector('.myrow');
 //console.log(myRowEl);
+const modalEl = document.getElementById('myModal');
+const modalImgEl = document.getElementById('modalImage');
+const modalTitleEl = document.getElementById('modalTitle');
 
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
-.then(response =>{
-    const postIts = response.data
-    //console.log(postIts);
+    .then(response => {
+        const postIts = response.data
+        //console.log(postIts);
 
-    let postItsEl = ''
-    postIts.forEach(postIt => {
-        
-        const {url, title} = postIt
+        let postItsEl = ''
+        postIts.forEach(postIt => {
+
+            const { url, title } = postIt
 
 
-        const markup =  ` 
+            const markup = ` 
         <div class="col-xs-12 col-lg-4 col-xx-4 postit">
             <div class="pin"><img src="./assets/img/pin.svg" alt=""></div>
             <img src="${url}" alt="">
             <div class="div cardtext">${title}</div>
         </div>
         `
-        postItsEl += markup
-    });
+            postItsEl += markup
+        });
         myRowEl.innerHTML = postItsEl
         //console.log(postEl);
-    
-       
-})
+  
 
+    })
+
+  
+    function showModal(url, title) {
+        modalEl.style.display = 'block';
+    }
+    
+    myRowEl.addEventListener('click', showModal);
